dce494ac5cf29ceb2fb0da0c4e8e466c879ddcc9
Rifkhan <rifkhan561@gmail.com>
2024-03-30 14:29:52 +0530

    update public key

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDuAZxJJjrlCoinfhuM5O3BRSNYoVRoEwN5c3iGoNGeR/yKtUfB35ytpbVmtd2gpGlqBCwmP00FWwQvjIUjypO8hyk8utElHmCvftEzZOgOZlIOoSO7SpCrse4wpxfLNakM45sXpcIv/3zf7hltEeLCnrRi3Q/6UVkzy08x5sF1K7MpUX72BdIL42p0PBO3D5oN+d8pZZjj569+waP4z7OHvSzU5QlYMSOiAzcSdwXL+jc9OfXGJ3d61xaxI1QhWScfBjgDPZlTNHKbsSC8tw8iICnYkzmm+W40gq+An0xgua149wzWN5Nuz71PBE+GSObgRdoOGWWpq1SrzJlo3LoyeD1crvpDeo8LzXOqMIC1HSG7ku5C+X+NbioU11QIkty78tuhHE7SLmtomgplaY3fi+t+qhdaIOvR3QOfyqbdTYgF499q/e1UX8K+penzWuCkAMV/Ktflbl2k6gd0uWwWcPVxc2amVjmm7rcRhPS5bF7YmNFyq572klzOI6xnw9k= ubuntu@Rifkhan"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_security_group" "allow_web" {
